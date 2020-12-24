d80387896d21ac796c316acf94076e79eae604b4
bionic790 <53811382+bionic790@users.noreply.github.com>
2020-12-24 12:35:42 +0530

    Update code.cpp

--- leaky bucket/code.cpp
+++ leaky bucket/code.cpp
@@ -1,29 +1,37 @@
-#include<iostream.h>
-#include<dos.h>
-#include<stdlib.h>
-#define bucketSize 512
-void bktInput(int a,int b) {
-if(a>bucketSize)
- cout<<"\n\t\tBucket overflow";
-else {
- delay(500);
- while(a>b){
-cout<<"\n\t\t"<<b<<" bytes outputted.";
-a-=b;
-delay(500);
- }
- if (a>0) cout<<"\n\t\tLast "<<a<<" bytes sent\t";
- cout<<"\n\t\tBucket output successful";
- }
+#include<bits/stdc++.h>
+#include<unistd.h>
+using namespace std;
+#define bucketSize 500
+
+void bucketInput(int a,int b) 
+{
+	if(a > bucketSize)
+		cout<<"\n\t\tBucket overflow";
+	else{
+		sleep(5);
+		while(a > b){
+			cout<<"\n\t\t"<<b<<" bytes outputted.";
+			a-=b;
+			sleep(5);
+		}
+		if(a > 0) 
+			cout<<"\n\t\tLast "<<a<<" bytes sent\t";
+		cout<<"\n\t\tBucket output successful";
+	}
 }
-void main( ) {
-int op, pktSize;
- randomize();
- cout<<"Enter output rate : "; cin>>op;
-for(int i=1;i<=5;i++){
- delay(random(1000));
- pktSize=random(1000);
- cout<<"\nPacket no "<<i<<"\tPacket size = "<<pktSize;
- bktInput(pktSize,op);
- }
+int main() 
+{
+	int op,pktSize;
+	cout<<"Enter output rate : "; 
+	cin>>op;
+    
+	for(int i=1;i<=5;i++)					
+	{
+		sleep(rand()%10);
+		pktSize=rand()%700;
+		cout<<"\nPacket no "<<i<<"\tPacket size = "<<pktSize;
+		bucketInput(pktSize,op);
+	}
+	cout<<endl;
+	return 0;
 }
